c39a57efb9c7a2b6caf8b581e435d5c5e37f10c4
Iasmim Siqueira <iasmimsas@gmail.com>
2024-03-10 11:47:10 -0300

    first commit

---
+++ src/main/java/br/ifpb/iasmim/financafacil/FinancaFacilApplication.java
@@ -0,0 +1,13 @@
+package br.ifpb.iasmim.financafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancaFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifpb/iasmim/financafacil/FinancaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifpb.iasmim.financafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
